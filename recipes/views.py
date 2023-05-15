b2b1ac038d219c626f2ee75ab1f93aca9b9399e1
mateusalvesj3050@gmail.com <mateusalvesj3050@gmail.com>
2023-05-15 10:10:29 -0300

    Usar get_list_or_404 ou não?

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-# from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(
-        is_published=True
+        is_published=True,
     ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
@@ -13,14 +12,6 @@ def home(request):
 
 
 def category(request, category_id):
-    # recipes = Recipe.objects.filter(
-    #     category__id=category_id,
-    #     is_published=True
-    # ).order_by('-id')
-
-    # if not recipes:
-    #     raise Http404('Not found')
-
     recipes = get_list_or_404(Recipe.objects.filter(
             category__id=category_id, is_published=True,
             ).order_by('-id')
