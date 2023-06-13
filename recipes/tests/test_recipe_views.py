26761e2b41ee484e912d9be0376bcc1582a32777
mateusalvesj3050@gmail.com <mateusalvesj3050@gmail.com>
2023-06-13 11:06:52 -0300

    Criando uma classe de base para os testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,11 +1,13 @@
-from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
-from recipes.models import Category, Recipe
-from django.contrib.auth.models import User
+
+from .test_recipe_base import RecipeTestBase
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
+    def tearDown(self) -> None:
+        return super().tearDown()
+
     def test_recipe_home_biew_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -19,6 +21,7 @@ class RecipeViewsTest(TestCase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        Recipe.objects.get(pk=1).delete()
         response = self.client.get(reverse('recipes:home'))
         self.assertIn(
             '<h1>No recipes found here 😥</h1>',
@@ -26,29 +29,6 @@ class RecipeViewsTest(TestCase):
         )
 
     def test_recipe_home_template_loads_recipe(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='User',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@gmail.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit=' Minutos',
-            servings=5,
-            servings_unit=' Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
-
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
