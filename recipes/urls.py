0996790207c5fde2be0a6f85fc09fa1c3c71fcd2
mateusalvesj3050@gmail.com <mateusalvesj3050@gmail.com>
2023-07-10 12:05:35 -0300

    Criando uma nova URL para search com TDD

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('recipes/category/<int:category_id>/', views.category, name="category"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),  # noqa: E501;
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/search', lambda request: ..., name="search"),
 ]
