b0fbacc664648376ea03f6ef6f22bf1998c39912
mateusalvesj3050@gmail.com <mateusalvesj3050@gmail.com>
2023-07-14 12:14:08 -0300

    Levantando erro 404 se o usuário não enviar o termo de busca

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -135,5 +135,6 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIs(resolved.func, views.search)
 
     def test_recipe_search_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:search'))
-        self.assertTemplateUsed(response, 'recipes/pages/search.html')
+        url = reverse('recipes:search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
