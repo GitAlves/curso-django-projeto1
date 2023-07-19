16c84a8b1af25a575e0ac56b3fb40615f3b7d039
mateusalvesj3050@gmail.com <mateusalvesj3050@gmail.com>
2023-07-19 09:35:58 -0300

    Testando se o search_term está escapando e criando um valor nulo para o uso de strip

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -139,6 +139,14 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
 
     def test_recipe_search_raises_404_if_no_search_term(self):
-        url = reverse('recipes:search')
+        url = reverse('recipes:search') + '?q= '
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_term_is_on_page_title_and_escape(self):
+        url = reverse('recipes:search') + '?q=<Teste>'
+        response = self.client.get(url)
+        self.assertIn(
+            'Search for &quot;&lt;Teste&gt;&quot;',
+            response.content.decode('utf-8')
+        )
