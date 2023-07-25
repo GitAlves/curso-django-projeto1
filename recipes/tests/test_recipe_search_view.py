47177e0eadeb97c4bbdef269ccbbb66e0d6f9c84
mateusalvesj3050@gmail.com <mateusalvesj3050@gmail.com>
2023-07-25 11:01:38 -0300

    Creating a white space by default on search bar

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -15,7 +15,7 @@ class RecipeDetailViewTest(RecipeTestBase):
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
 
     def test_recipe_search_raises_404_if_no_search_term(self):
-        url = reverse('recipes:search') + '?q= '
+        url = reverse('recipes:search')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
